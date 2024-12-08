60bf76254485434d088b56b183d62bc49c97a4b9
BOGDAN <bogdan.pirtica@student.upt.ro>
2024-12-08 20:34:57 +0200

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+//package org.example;
+
+class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 2;
+        b = 3;
+        c = 4;
+        if(a > b) {
+            System.out.println(a + b + c);
+        }
+    }
+}
